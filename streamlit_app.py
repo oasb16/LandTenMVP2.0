8d34b233d8248b3e78f2a6eeb08d2cfddb3dc063
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-04-03 20:59:12 -0700

    Init Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from superstructures.ss1_personagate.run import run_persona_gate
-from superstructures.ss2_sessionrouter.run import run_session_router
-from superstructures.ss3_trichatcore.run import run_chat_core
-from superstructures.ss4_agenttoggle.run import run_agent_toggle
-from superstructures.ss5_summonengine.run import run_summon_engine
-from superstructures.ss6_actionrelay.run import run_action_relay
+from superstructures.ss1_personagate.personagate import run_persona_gate
+from superstructures.ss2_sessionrouter.session_router import run_session_router
+from superstructures.ss3_trichatcore.tri_chat_core import run_chat_core
+from superstructures.ss4_agenttoggle.agent_toggle_ui import run_agent_toggle
+from superstructures.ss5_summonengine.summon_engine import run_summon_engine
+from superstructures.ss6_actionrelay.actionrelay import run_action_relay
 
 st.set_page_config(page_title="TriChatLite", layout="wide")
 
