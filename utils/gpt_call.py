c715cbd0edcac95af7871b710fdcc4a148076d92
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-04-24 17:17:04 -0700

    Init Commit

--- utils/gpt_call.py
+++ utils/gpt_call.py
@@ -8,7 +8,11 @@ import streamlit as st
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 def call_gpt_agent(chat_log):
-    messages = [{"role": "system", "content": "You are a helpful agent for property issues."}]
+    messages = [{"role": "system", "content": "You are a helpful agent for property issues. Figure out the best way to help the user in just 1 response. \n"
+    "You are a helpful assistant for property issues. You can help the user by providing information, answering questions, or suggesting solutions. \n"
+    "You can also ask the user for more information if needed. \n"
+    "You can also ask the user to upload a media of the issue if needed. \n"
+    "You have only 20 words to respond. \n"}]
     for msg in chat_log:
         if msg["role"] in ["tenant", "landlord", "contractor"]:
             messages.append({"role": "user", "content": msg["message"]})
