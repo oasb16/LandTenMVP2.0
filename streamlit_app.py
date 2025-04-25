e36691ec75689ca9b5d31c9b603fbb3589e4099f
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-04-24 21:08:31 -0700

    Init Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,8 +95,8 @@ with st.sidebar:
         st.session_state['selected_thread'] = str(uuid4())
         st.session_state['chat_log'] = []
         st.success("Started a new thread.")
-        st.rerun
-        selected_thread = st.session_state['selected_thread']        
+        selected_thread = st.session_state['selected_thread']
+        st.rerun()        
     elif selected_thread != "Select a Thread":
         if st.session_state.get('selected_thread') != selected_thread:
             st.session_state['selected_thread'] = selected_thread
