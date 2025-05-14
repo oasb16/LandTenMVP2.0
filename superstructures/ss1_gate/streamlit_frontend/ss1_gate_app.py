5eb5e88382785f62e1876d31b4932c872e2de915
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-05-14 01:23:25 -0500

    Init Commit

--- superstructures/ss1_gate/streamlit_frontend/ss1_gate_app.py
+++ superstructures/ss1_gate/streamlit_frontend/ss1_gate_app.py
@@ -46,7 +46,7 @@ def run_login():
 
         st.link_button(
             "🔐 Login with GOOOOGLE SSO",
-            f"https://landten-login-redirect.s3-website-us-east-1.amazonaws.com/login-redirect.html?state={st.session_state["persona"]}"
+            "http://landten-login-redirect.s3-website-us-east-1.amazonaws.com/login-redirect.html"
         )
         st.stop()
 
