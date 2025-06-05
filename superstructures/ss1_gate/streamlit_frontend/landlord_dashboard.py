21a7f66344469e59f0365bc092889c73e7776997
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-06-05 12:53:32 -0500

    Init Commit

--- superstructures/ss1_gate/streamlit_frontend/landlord_dashboard.py
+++ superstructures/ss1_gate/streamlit_frontend/landlord_dashboard.py
@@ -16,8 +16,8 @@ from streamlit.components.v1 import html
 from utils.dev_tools import list_json_objects, load_json_from_s3
 import pandas as pd
 import math
-from superstructures.ss7_intelprint.report_engine import generate_pdf_report
-from ss5_summonengine.chat_summarizer import summarize_chat_thread
+# from superstructures.ss7_intelprint.report_engine import generate_pdf_report
+# from ss5_summonengine.chat_summarizer import summarize_chat_thread
 # from superstructures.ss6_actionrelay.job_manager import create_job
 
 # -- Core Modules --
